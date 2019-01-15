2ad600dd2a92427257d044f7d6c89ca43c0df8ae
Sebastien Dubois <lechtitseb@gmail.com>
2019-01-15 13:07:29 +0100

    Improved 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,20 +2,17 @@ import * as React from 'react';
 
 import Layout from '../components/layout';
 
-// FIXME change export default
-// -> maybe also switch to class
-const NotFoundPage = () => (
+export default () => (
   <Layout>
     <div className="main alt">
         <section id="one">
             <div className="inner">
                 <h1>Page not found :(</h1>
                 <p>The page you were looking for could not be found, sorry!</p>
-              <p>Why don't you go back <a href="/">home</a>?</p>
+              <p>Why don't you go back?</p>
+              <p><a href="/" className="button fit">HOME</a></p>
             </div>
         </section>
     </div>
   </Layout>
 );
-
-export default NotFoundPage
